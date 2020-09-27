fb725e980f4447861a4b28a09a2379750601c6d1
Nipul Singal <nipul00rock@gmail.com>
2020-09-27 21:55:35 +0530

    Minor Updates

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://nipul408:nipul221100@cluster0.pttqq.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  secretOrKey: "mysecretkey"
+  mongoURI: "",
+  secretOrKey: ""
 };
